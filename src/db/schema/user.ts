e042e5809c6f54c6e2dcadf98e61f3ae12825926
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-25 19:30:12 -0300

    export user schema

---
+++ src/db/schema/user.ts
@@ -0,0 +1,23 @@
+import {
+	boolean,
+	pgTable,
+	timestamp,
+	uuid,
+	varchar,
+} from "drizzle-orm/pg-core";
+import { enumToPgEnum } from "../../common/utils/enum.util";
+import { EUserRoles } from "../../common/enums/user.enum";
+
+export const users = pgTable("users", {
+	id: uuid("id").defaultRandom().primaryKey(),
+	emai: varchar("email", { length: 255 }).notNull().unique(),
+	password: varchar("password", { length: 255 }).notNull(),
+	firstName: varchar("first_name", { length: 100 }).notNull(),
+	lastName: varchar("last_name", { length: 100 }).notNull(),
+	role: varchar("role", { enum: enumToPgEnum(EUserRoles) }).notNull(),
+	isActive: boolean("is_active").notNull().default(true),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export type TUser = typeof users.$inferSelect;
