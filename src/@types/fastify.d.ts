f9b5fc0cccfc52052a4063808cb453e830c6c78b
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-08-26 18:34:07 -0300

    fix pre handler interface

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -1,21 +1,13 @@
+import type {
+	FastifyReply,
+	FastifyRequest,
+	RouteGenericInterface,
+} from "fastify";
 import type { JWT } from "@fastify/jwt";
 import { EUserRoles } from "../common/enums/user.enum";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { schemas } from "../db/schema";
 import { MainContainer } from "../container";
-
-declare module "fastify" {
-	interface FastifyRequest {
-		jwt: JWT;
-		user: UserPayload;
-	}
-
-	interface FastifyInstance {
-		authenticate: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
-		db: NodePgDatabase<typeof schemas>;
-		container: MainContainer;
-	}
-}
 
 type UserPayload = {
 	id: string;
@@ -24,6 +16,22 @@ type UserPayload = {
 	role: EUserRoles;
 	isVerified: boolean;
 };
+
+declare module "fastify" {
+	interface FastifyRequest {
+		jwt: JWT;
+		user?: UserPayload;
+	}
+
+	interface FastifyInstance {
+		authenticate<T extends RouteGenericInterface = RouteGenericInterface>(
+			req: FastifyRequest<T>,
+			reply: FastifyReply
+		): void | Promise<void>;
+		db: NodePgDatabase<typeof schemas>;
+		container: MainContainer;
+	}
+}
 
 declare module "@fastify/jwt" {
 	interface FastifyJWT {
